1809c8d75c20aa330cd6823f833b0950d52748c9
pedrooliveira16 <pedro.oliv16@gmail.com>
2019-12-20 16:13:20 +0000

    Add multi-subreddit and multi-line support

--- bot.js
+++ bot.js
@@ -1,7 +1,12 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-var req = require('request');
+var Discord = require('discord.io')
+var logger = require('winston')
+var auth = require('./auth.json')
+var req = require('request')
+var cron = require('node-cron')
+const readline = require('readline')
+const fs = require('fs')
+const subredditsFile = './presenceSubreddits.txt'
+var subreddits = []
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -9,17 +14,42 @@ logger.add(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+
+//populate the random presence subreddits array from file
+async function populateSubredditArray(){
+    try{
+        await fs.promises.access(subredditsFile);
+        cron.schedule("*/30 * * * *", randomSubredditPresence)
+        const readInterface = readline.createInterface({
+            input: fs.createReadStream(subredditsFile),
+            console: false
+        });
+        readInterface.on('line', function(line){
+            subreddits.push(line)
+        });
+    }
+    catch(error){
+        console.log("Subreddits file does not exist, randomPresence disabled")
+    }
+}
+//change subreddit presence to a random subreddit from a list
+function randomSubredditPresence(){
+    chosen = subreddits [Math.floor(Math.random()*subreddits.length)]
+    bot.setPresence({ status: 'online', game: { type: 3, name: chosen, url: "https://reddit.com" } });
+}
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
-
     bot.setPresence({ status: 'online', game: { name: "Let's get physical" } });
+    populateSubredditArray()
 });
 
 bot.on('message', function (user, userID, channelID, message, evt){
@@ -31,20 +61,19 @@ bot.on('message', function (user, userID, channelID, message, evt){
         bot.setPresence({ status: 'online', game: { name: presence } });
     }
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-    if (user!= bot.username && message.indexOf("/r/") > -1 && message.indexOf("hub.docker.com") == -1 ) {
-        var lines = message.split('\n')
-        for(var l=0;l<lines.length;l++){
-            var str=lines[l];
-            if(str.indexOf("/r/")>-1 && str.indexOf("reddit.com")==-1){
-                var regexp = /^.*\/r\/([a-zA-Z_0-9]+).*$/;
-                var subreddit = str.match(regexp)[1];
 
+bot.on('message', function (user, userID, channelID, message, evt) {
+    if (user != bot.username) {
+        let regexp = /(?:^|\s)\/?r\/([a-zA-Z_0-9]+)/gm
+        let match = [...message.matchAll(regexp)]
+        if(Array.isArray(match) && match.length){
+            for(let i=0;i<match.length;i++){
+                let subreddit = match[i][1];
                 req.get('https://www.reddit.com/api/search_reddit_names.json?query='+subreddit+'&exact=True', function (error, response, body) {
                     if(error){
                         logger.error(error);
                     }
-                    var json=JSON.parse(body);
+                    let json=JSON.parse(body);
                         if(json.names == undefined || json.names.length==0){
                             bot.sendMessage({
                                 to: channelID,
@@ -52,7 +81,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                             });
                         }
                         else{
-                            var link= "https://www.reddit.com/r/"+subreddit;
+                            let link= "https://www.reddit.com/r/"+subreddit;
                             bot.sendMessage({
                                 to: channelID,
                                 message: link
@@ -61,5 +90,5 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
             }
         }
-     }
+    }
 });
